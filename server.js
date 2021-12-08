756bb400a7420e0798509d789c1bbc37b7382f54
Ryan Small <ryan.small@feapc.com>
2021-12-08 15:11:51 -0500

    wip: adding heroku

--- server.js
+++ server.js
@@ -1,19 +1,60 @@
-// require dependencies
+//___________________
+//Dependencies
+//___________________
 const express = require("express");
-
-// initialize the express app
+const methodOverride = require("method-override");
+const mongoose = require("mongoose");
 const app = express();
+const db = mongoose.connection;
+require("dotenv").config();
+//___________________
+//Port
+//___________________
+// Allow use of Heroku's port or your own local port, depending on the environment
+const PORT = process.env.PORT || 3000;
 
-// config settings
-const PORT = 3000;
+//___________________
+//Database
+//___________________
+// How to connect to the database either via heroku or locally
+const MONGODB_URI = process.env.MONGODB_URI;
 
-// connect to mongo db
-
-// mount middleware
-
-// mount routes
-
-// tell app to listen
-app.listen(PORT, () => {
-    console.log(`Express is listening on port: ${PORT}`);
+// Connect to Mongo &
+// Fix Depreciation Warnings from Mongoose
+// May or may not need these depending on your Mongoose version
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
+
+// Error / success
+db.on("error", (err) => console.log(err.message + " is mongodb not running?"));
+db.on("connected", () => console.log("mongodb connected: ", MONGODB_URI));
+db.on("disconnected", () => console.log("mongodb disconnected"));
+
+//___________________
+//Middleware
+//___________________
+
+//use public folder for static assets
+app.use(express.static("public"));
+
+// populates req.body with parsed info from forms - if no data from forms will return an empty object {}
+app.use(express.urlencoded({ extended: false })); // extended: false - does not allow nested objects in query strings
+app.use(express.json()); // returns middleware that only parses JSON - may or may not need it depending on your project
+
+//use method override
+app.use(methodOverride("_method")); // allow POST, PUT and DELETE from a form
+
+//___________________
+// Routes
+//___________________
+//localhost:3000
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+//___________________
+//Listener
+//___________________
+app.listen(PORT, () => console.log("express is listening on:", PORT));
