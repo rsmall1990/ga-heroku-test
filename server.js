16e600b0407514c66d295c832f504b388cf4f907
Ryan Small <ryan.small@feapc.com>
2021-12-07 22:06:16 -0500

    wip: add first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+// require dependencies
+const express = require("express");
+
+// initialize the express app
+const app = express();
+
+// config settings
+const PORT = 3000;
+
+// connect to mongo db
+
+// mount middleware
+
+// mount routes
+
+// tell app to listen
+app.listen(PORT, () => {
+    console.log(`Express is listening on port: ${PORT}`);
+});
